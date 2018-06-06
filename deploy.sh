0dfbfaf7965726cb5969c8deb0a60b0fd5471ebf
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-06-06 17:43:36 +1200

    Expose TRAVIS_TEST_RESULT

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ test ! -z $CC_TEST_REPORTER_ID
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
 ./cc-test-reporter before-build
-./build.sh
+. ./build.sh
 ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
 
 pip install awscli --upgrade --user
