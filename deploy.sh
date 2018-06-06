920cc90f06ea565a5d2116a4396afda698532369
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-06-06 17:42:30 +1200

    Factor out deploy script, DRY creds

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+set -eux
+
+test ! -z $TRAVIS
+test ! -z $AWS_ACCESS_KEY_ID
+test ! -z $AWS_SECRET_ACCESS_KEY
+test ! -z $CC_TEST_REPORTER_ID
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+./build.sh
+./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+
+pip install awscli --upgrade --user
+
+aws s3 sync --region us-west-2 \
+  dist/doc \
+  s3://aaronjameslang.com/survey-monkey-streams
+
+aws s3 sync --region us-west-2 \
+  coverage/lcov-report \
+  s3://aaronjameslang.com/survey-monkey-streams/coverage
